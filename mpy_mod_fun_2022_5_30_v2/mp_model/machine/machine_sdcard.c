ede8feb1bb01a0a9bd969d006ea7f3fdd7ab8ded
starraiser <490349699@qq.com>
2023-02-01 13:26:30 +0000

    update license
    Signed-off-by: starraiser <490349699@qq.com>

--- mpy_mod_fun_2022_5_30_v2/mp_model/machine/machine_sdcard.c
+++ mpy_mod_fun_2022_5_30_v2/mp_model/machine/machine_sdcard.c
@@ -3,8 +3,7 @@
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2017 "Eric Poulsen" <eric@zyxod.com>
- * Copyright (c) 2017 "Tom Manning" <tom@manningetal.com>
+ * Copyright (c) 2019 Nicko van Someren
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
